7fe52c6370962120129ca3e0495b91c8ebebf2e6
Mannam Akhil <mannamakhil@gmail.com>
2021-04-17 00:18:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -97,11 +97,12 @@ app.post('/urls/:id', authenticate, async (req, res) => {
     try {
         let connection = await mongodb.connect(URL, {useUnifiedTopology : true});
         let db = connection.db(DB);
-        await db.collection('users').updateOne({_id : mongodb.ObjectID(req.params.id)},{$push : {links : {$each : [{longURL : req.body.longURL , shortURL : nanoid(6)}]}}});
+        let response = await db.collection('users').updateOne({_id : mongodb.ObjectID(req.params.id)},{$push : {links : {$each : [{longURL : req.body.longURL , shortURL : nanoid(6)}]}}});
         await connection.close();
-        res.json({
-            message : "URL Created"
-        })
+//         res.json({
+//             message : "URL Created"
+//         })
+        res.json(response);
     } catch (error) {
         console.log(error);
     }
